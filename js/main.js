26bf1eaac7e3e8f55cf64eb618699d61d4b6a8f6
coders-sohan <coders.sohan.0216@gmail.com>
2021-08-21 10:51:14 +0600

    complete assignment 05

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
+// change value
+
 function updateValue(id, value) {
 
-    var costElement = document.getElementById(id);
+    const costElement = document.getElementById(id);
     costElement.innerText = value;
+    calc();
 
+}
+
+// calculation total
+
+function calc() {
+   const mainPrice = document.getElementById('main-price').innerText;
+   const memoryCost = document.getElementById('memory-cost').innerText;
+   const storageCost = document.getElementById('storage-cost').innerText;
+   const deliveryCost = document.getElementById('delivery-cost').innerText;
+   const totalCost = parseInt(mainPrice) + parseInt(memoryCost) + parseInt(storageCost) + parseInt(deliveryCost);
+   document.getElementById('total-cost').innerText = totalCost;
+   document.getElementById('total-final').innerText = totalCost;
 }
 
 document.getElementById('memory-8gb').addEventListener('click', function () {
@@ -31,4 +46,18 @@ document.getElementById('delivery-free').addEventListener('click', function () {
 
 document.getElementById('delivery-paid').addEventListener('click', function () {
    updateValue('delivery-cost', 20); 
+});
+
+// coupon code calc
+
+document.getElementById('promo-btn').addEventListener('click', function () {
+   const promoInput = document.getElementById('promo-code');
+   if (promoInput.value == "stevekaku") {
+      const totalValue = document.getElementById('total-final');
+      const promoDiscount = totalValue.innerText*20/100;
+      console.log(promoDiscount);
+      totalValue.innerText = totalValue.innerText - promoDiscount;
+   }
+   document.getElementById('promo-btn').disabled = true;
+   promoInput.value = '';
 });
